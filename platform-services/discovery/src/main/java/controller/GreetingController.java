e90bce32c83a3ab9276a5987cf13387a556f382d
Iurii Vikhrov <iurii_vikhrov@epam.com>
2021-12-21 16:13:39 +0300

    initial commit

---
+++ platform-services/discovery/src/main/java/controller/GreetingController.java
@@ -0,0 +1,9 @@
+package com.example.discovery.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+
+    @RequestMapping("/greeting")
+    String greeting();
+}
