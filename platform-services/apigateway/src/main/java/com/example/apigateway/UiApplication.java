7971539d0f4b7c0a8241d0042d8c22bc0cf03879
Yuriy <dpsofjg@rambler.ru>
2022-01-06 14:23:35 +0300

    доработки Zuul и Docker

--- platform-services/apigateway/src/main/java/com/example/apigateway/UiApplication.java
+++ platform-services/apigateway/src/main/java/com/example/apigateway/UiApplication.java
@@ -3,10 +3,12 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class UiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
